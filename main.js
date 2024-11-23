309f62c5cbd98679e9e59df25bd8ad6f56312da5
Shannnon Evora Lopes Rodrigues <shannonelr9208@icloud.com>
2024-11-23 16:20:40 +0100

    header is comming....

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ const navLink = document.querySelector('.nav-links');
 burgerIcon.addEventListener('click',() =>{
     navLink.classList.toggle('active');
 });
+
+//Cerramos el menú al hacer click sobre un enlace
+document.querySelectorAll('.nav-links a').forEach(link =>{
+    link.addEventListener('click',()=> {
+        if (window,innerWidth <= 768){
+            navLink.classList.remove('active');
+       }
+    });
+});
